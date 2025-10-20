72fee2a9e095e96ffe16739aab1d86c5303089ba
Veyra <veyra0047@gmail.com>
2025-10-20 21:58:54 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import FeaturesSection from "@/components/FeaturesSection";
 import AboutPreviewSection from "@/components/AboutPreviewSection";
 import WhyChooseReviveSection from "@/components/WhyChooseReviveSection";
 
-export default function HomePage(): JSX.Element {
+export default function HomePage() {
   return (
     <main className="relative">
       <Navbar />
